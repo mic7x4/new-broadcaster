bd67da3352ad8bab8dcfa33f2e6f87bb4959bfcb
mic7x4 <mic7x4@gmail.com>
2021-01-27 17:43:07 +0200

    feature(server) user can create an account

---
+++ server/Model/Users.js
@@ -0,0 +1,34 @@
+const Users = [
+    {
+        id:1,
+        firstname:"Ndabaga",
+        lastname:"Michel",
+        email:"mic7x4@gmail.com",
+        password:'crookzartwork',
+        phoneNumber:"0788663994",
+        username :'crook',
+        isAdmin:false
+    },
+    {
+        id:2,
+        firstname:"Bizagorana",
+        lastname:"Jean",
+        email:"bizagorana@.com",
+        password:'crookzartwork',
+        phoneNumber:"0788663994",
+        username:'bizagorana',
+        isAdmin:true
+    },
+    {
+        id:3,
+        firstname:"Uwimana",
+        lastname:"Emannuel",
+        email:"emmy@gmail.com",
+        password:'crookzartwork',
+        phoneNumber:"0788663990",
+        username:'username',
+        isAdmin:false
+    },
+]
+
+export default Users;
