1f68491b047b7e6be9e9928c7b621f296bcc4940
mic7x4 <mic7x4@gmail.com>
2021-02-01 13:04:52 +0200

    feature(Server)  user can get a single Record

--- server/controllers/recordControllers.js
+++ server/controllers/recordControllers.js
@@ -2,13 +2,14 @@ import Records from '../Model/Records.js'
 
 
 class recordControllers{
+    // View All records
     static getRecords(req,res){
         return res.status(200).json({
             message:"Record Founds",
             data:Records
         })
     }
-
+    // Create a new Record
     static createRecord(req,res){
         const {title,type,comment,location,status,images,videos} = req.body;
 
@@ -23,6 +24,18 @@ class recordControllers{
             message:'Record Created Successfully!',
             data:newRecord
         })
+    }
+    // Getting a single record
+    static getSingleRecord(req,res){
+        const foundRecord = Records.find((record)=>record.id === parseInt(req.params.id));
+        if(!foundRecord){
+            return res.status(400).json({
+                message:"Record not found"
+            });
+        }
+        return res.status(200).json({message:"Record found",
+        data:{status:res.statusCode,record:foundRecord}})
+    
     }
 }
 
