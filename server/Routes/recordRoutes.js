082e54016a19143848f278c8775a69da2887d771
mic7x4 <mic7x4@gmail.com>
2021-02-01 12:23:03 +0200

    feature(Server) user can create a redflag

---
+++ server/Routes/recordRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import recordControllers from '../controllers/recordControllers.js';
+
+
+const recordRoute = express.Router();
+//Getting all records
+recordRoute.post('/records',recordControllers.createRecord);
+recordRoute.get('/records',recordControllers.getRecords);
+
+
+export default recordRoute;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import usersRoutes from './Routes/usersRoutes.js'
+import usersRoutes from './Routes/usersRoutes.js';
+import recordRoute from './Routes/recordRoutes.js';
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', usersRoutes);
+app.use('/',recordRoute);
 
 
 const PORT = process.env.PORT || 8000;
